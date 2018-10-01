63ce1c6f1c4e13a5aa22e20b5380bae87f821281
Nikolai Åsen Magnussen <nikolai@magnussen.tf>
2018-10-01 15:34:43 +0200

    Make sure to flush to properly send out data over the network

--- Pwn/2. Privilege Escalation/src/privesc.c
+++ Pwn/2. Privilege Escalation/src/privesc.c
@@ -17,6 +17,7 @@ struct user {
 void print_menu(void)
 {
         printf("\nOptions:\n\t1) Read note\n\t2) Write note\n\t3) Print user info\n\t4) Read flag (privileged operation)\n\tq) Log out and quit\n\nEnter choice: ");
+	fflush(stdout);
 }
 
 int main(void)
@@ -43,9 +44,11 @@ int main(void)
 
 
         printf("Username: ");
+	fflush(stdout);
         if (fgets(username, 19, stdin) == NULL) {
-                fprintf(stderr, "Error reading username");
-                exit(EXIT_FAILURE);
+                fprintf(stderr, "Error reading username\n");
+		fflush(stderr);
+                exit(EXIT_SUCCESS);
         } else {
                 char *newline = strchr(username, '\n');
                 if (newline)
@@ -53,9 +56,11 @@ int main(void)
         }
 
         printf("Password: ");
+	fflush(stdout);
         if (fgets(password, 19, stdin) == NULL) {
-                fprintf(stderr, "Error reading password");
-                exit(EXIT_FAILURE);
+                fprintf(stderr, "Error reading password\n");
+		fflush(stderr);
+                exit(EXIT_SUCCESS);
         } else {
                 char *newline = strchr(password, '\n');
                 if (newline)
@@ -64,10 +69,12 @@ int main(void)
 
         if (strcmp(normal_user.username, username) != 0 ||
             strcmp(normal_user.password, password) != 0) {
-                fprintf(stderr, "Error, wrong username and password combination");
-                exit(EXIT_FAILURE);
+                fprintf(stderr, "Error, wrong username and password combination\n");
+		fflush(stderr);
+                exit(EXIT_SUCCESS);
         } else {
                 printf("\nWelcome to OneLineNote %s %s\n", normal_user.first_name, normal_user.last_name);
+		fflush(stdout);
         }
 
         char choice[2] = { 0 };
@@ -77,9 +84,11 @@ int main(void)
                 switch (choice[0]) {
                         case '1':
                                 printf("Note contents: %s\n", note);
+				fflush(stdout);
                                 break;
                         case '2':
                                 printf("Enter note: ");
+				fflush(stdout);
                                 __fpurge(stdin);
                                 fgets(note, 199, stdin);
 
@@ -89,7 +98,8 @@ int main(void)
                                     strcmp(normal_user.username, stored_username)     != 0 ||
                                     strcmp(normal_user.password, stored_password)     != 0) {
                                         fprintf(stderr, "Integrity check failed, overflow detected! Exiting..\n");
-                                        exit(EXIT_FAILURE);
+					fflush(stderr);
+                                        exit(EXIT_SUCCESS);
                                 }
                                         
                                 break;
@@ -100,6 +110,7 @@ int main(void)
                                                 normal_user.last_name,
                                                 normal_user.username,
                                                 normal_user.password);
+				fflush(stdout);
                                 break;
                         case '4':
                                 if (normal_user.privileged) {
@@ -112,14 +123,17 @@ int main(void)
                                         }
                                 } else {
                                         printf("You must be privileged to get the flag, but %s is not privileged!\n", normal_user.username);
+					fflush(stdout);
                                 }
                                 break;
                         case 'q':
                                 printf("Logging out and quitting application\n");
+				fflush(stdout);
                                 exit(EXIT_SUCCESS);
                         default:
                                 printf("Unknown choice, exiting..\n");
-                                exit(EXIT_FAILURE);
+				fflush(stdout);
+                                exit(EXIT_SUCCESS);
                 }
                 print_menu();
                 choice[0] = '\0';
