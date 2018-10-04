0b57b36f56d9d462976f281311b974fccccc8b70
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-10-04 17:56:06 +0200

    Add Empty files.net exercise

---
+++ Pwn/4. files.net/src/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UiTHack18.Pwn.FilesNet
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Files.NET!");
+            Console.WriteLine("A simple file sharing solution.");
+            Console.WriteLine("NOTE: This service is still heavily in development!");
+            Console.WriteLine();
+
+            Console.WriteLine("You can always enter the 'Help' action for more information.");
+
+        }
+    }
+}
